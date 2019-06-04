b28455caeac0d3b1e43cb16e1c767c625d49ae16
tiana-horn <tiana.horn@gmail.com>
2019-06-04 07:47:58 -0400

    Ignore special characters

--- src/Words.java
+++ src/Words.java
@@ -46,7 +46,6 @@ public class Words {
 			
 			// ignore special characters
 			String spec[] = {"!", "@", "#", "$", "%", "^", "&", "*", "•", "-", ",","","/","=", "//", "{", "("};
-			
 			for(String c:spec) {
 				wordCount.remove(c);
 			}
